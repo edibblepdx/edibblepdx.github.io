e1bf2fc97dd32c157a29ba3563781c77fdfa9445
ethan dibble <edibble@pdx.edu>
2025-01-06 21:17:11 -0800

    padded href

--- scripts/toc.js
+++ scripts/toc.js
@@ -1,20 +1,35 @@
 // https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/
 window.addEventListener('DOMContentLoaded', () => {
 
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const id = entry.target.getAttribute('id');
-      if (entry.intersectionRatio > 0) {
-        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-      } else {
-        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-      }
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            if (entry.intersectionRatio > 0) {
+                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+            } else {
+                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+            }
+        });
     });
-  });
 
-  // Track all sections that have an `id` applied
-  document.querySelectorAll('section[id]').forEach((section) => {
-    observer.observe(section);
-  });
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section[id]').forEach((section) => {
+        observer.observe(section);
+    });
   
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Scroll to the target element with an offset
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    });
 });
