695c4d051a3235b92f9e7dc296a07143f752ab9d
ethan dibble <edibble@pdx.edu>
2025-01-11 20:24:17 -0800

    script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
 # move changes into deployment branch
-git checkout -b gh-pages
-git merge main
+git checkout gh-pages && git merge main
 
-# move all files into .gitignore
-: > .gitignore
-for filename in *; do
-    echo "$filename" >> .gitignore
-done
+if [[ "$(git branch --show-current)" == "gh-pages" ]]; then
+    # move all files into .gitignore
+    : > .gitignore
+    for filename in *; do
+        echo "$filename" >> .gitignore
+    done
 
-# move distribution files into root
-mv dist/* .
+    # move distribution files into root
+    mv dist/* .
 
-# stage and push files
-git add --all
-git commit -m "$(date)"
-git push -u origin gh-pages
+    # stage and push files
+    git add --all \
+    && git commit -m "$(date)" \
+    && git push -u origin gh-pages
 
-# go back to main branch
-git checkout main
+    # go back to main branch
+    git checkout main
+    echo "SUCCESS"
+else
+    echo "FAILURE"
+    exit 1
+fi
