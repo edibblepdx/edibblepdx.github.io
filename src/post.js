5150b654e48f0e79b1f52aee59b00fc5fb9d1e93
ethan dibble <edibble@pdx.edu>
2025-10-07 15:22:12 -0700

    proper scaling on phones and misc fixes

--- src/post.js
+++ src/post.js
@@ -8,77 +8,77 @@ import fm from 'front-matter';
 const marked = new Marked();
 /* syntax highlighting */
 marked.use(markedHighlight({
-        emptyLangClass: 'hljs',
-        langPrefix: 'hljs language-',
-        highlight(code, lang, info) {
-            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-            return hljs.highlight(code, { language }).value;
-        }
-    })
+  emptyLangClass: 'hljs',
+  langPrefix: 'hljs language-',
+  highlight(code, lang, info) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+    return hljs.highlight(code, { language }).value;
+  }
+})
 );
 /* LaTeX parsing */
 marked.use(markedKatex({
-        throwOnError: false
-    })
+  throwOnError: false
+})
 );
 /* header id's and toc postprocessing hook */
 const options = {
-    hooks: {
-        postprocess(html) {
-            const headings = getHeadingList();
-            headings.map(({id, raw, level}) => {
-                const template = document.getElementById('tocItem');
-                const clone = template.content.cloneNode(true);
+  hooks: {
+    postprocess(html) {
+      const headings = getHeadingList();
+      headings.map(({ id, raw, level }) => {
+        const template = document.getElementById('tocItem');
+        const clone = template.content.cloneNode(true);
 
-                const anchor = clone.querySelector('a');
-                anchor.href = '#' + id;
-                anchor.textContent = raw;
+        const anchor = clone.querySelector('a');
+        anchor.href = '#' + id;
+        anchor.textContent = raw;
 
-                const li = clone.querySelector('li');
-                li.className = 'h' + level;
+        const li = clone.querySelector('li');
+        li.className = 'h' + level;
 
-                document.getElementById("tocList").appendChild(clone);
-            });
-            return html;
-        }
+        document.getElementById("tocList").appendChild(clone);
+      });
+      return html;
     }
+  }
 }
 
 const event = new Event('custom-content-loaded');
 let params = new URLSearchParams(document.location.search);
 let title = params.get("title");
 if (title) {
-    fetch('posts/' + title  + '.md')
-        .then((response) => {
-            return response.text();
-        })
-        .then((text) => {
-            let content = fm(text);
+  fetch('posts/' + title + '.md')
+    .then((response) => {
+      return response.text();
+    })
+    .then((text) => {
+      let content = fm(text);
 
-            /* title and date */
-            document.getElementById('title').innerHTML = 
-                marked.parse(
-                    '# ' + content.attributes.title + '\n\n' + 
-                    '### ' + content.attributes.date
-                );
-            
-            /* (body only) id headings and apply postprocessing */
-            marked.use(gfmHeadingId({prefix: "section-"}), options);
+      /* title and date */
+      document.getElementById('title').innerHTML =
+        marked.parse(
+          '# ' + content.attributes.title + '\n\n' +
+          '### ' + content.attributes.date
+        );
 
-            /* body */
-            document.getElementById('content').innerHTML = 
-                marked.parse(content.body);
+      /* (body only) id headings and apply postprocessing */
+      marked.use(gfmHeadingId({ prefix: "section-" }), options);
 
-            /* let other scripts know that the parser has finished */
-            dispatchEvent(event); 
-        })
-        .catch((err) => {
-            document.getElementById('content').innerHTML = 
-                marked.parse('# File Not Found');
-            console.error(err);
-        })
+      /* body */
+      document.getElementById('content').innerHTML =
+        marked.parse(content.body);
+
+      /* let other scripts know that the parser has finished */
+      dispatchEvent(event);
+    })
+    .catch((err) => {
+      document.getElementById('content').innerHTML =
+        marked.parse('# File Not Found');
+      console.error(err);
+    })
 } else {
-    document.getElementById('content').innerHTML = 
-        marked.parse('# File NULL');
-    console.error("ERROR::FILE::NULL");
+  document.getElementById('content').innerHTML =
+    marked.parse('# File NULL');
+  console.error("ERROR::FILE::NULL");
 }
--- src/toc.js
+++ src/toc.js
@@ -2,39 +2,39 @@
 // https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/
 window.addEventListener('custom-content-loaded', () => {
 
-    let activeElement = null;
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-            const navElement = document.querySelector(`nav li a[href="#${id}"]`)
-            if (entry.isIntersecting) {
-                if (activeElement && activeElement !== navElement) {
-                    activeElement.parentElement.classList.remove('active');
-                }
-                navElement.parentElement.classList.add('active');
-                activeElement = navElement;
-            }
-        });
+  let activeElement = null;
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      const navElement = document.querySelector(`nav li a[href="#${id}"]`)
+      if (entry.isIntersecting) {
+        if (activeElement && activeElement !== navElement) {
+          activeElement.parentElement.classList.remove('active');
+        }
+        navElement.parentElement.classList.add('active');
+        activeElement = navElement;
+      }
     });
+  });
 
-    // Track all headers that have an `id` applied
-    document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach((header) => {
-        observer.observe(header);
+  // Track all headers that have an `id` applied
+  document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach((header) => {
+    observer.observe(header);
+  });
+
+  // add padding above sections when jumped to
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      // Scroll to the target element with an offset
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+        top: targetElement.offsetTop - 150,
+        behavior: 'smooth'
+      });
     });
-
-    // add padding above sections when jumped to
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            // Scroll to the target element with an offset
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            
-            window.scrollTo({
-                top: targetElement.offsetTop - 150,
-                behavior: 'smooth'
-            });
-        });
-    });
+  });
 });
